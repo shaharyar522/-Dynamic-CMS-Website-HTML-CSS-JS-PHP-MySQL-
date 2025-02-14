23eaba7067d9cc8a2566bcb50f4ca7f0ccbcdedf
M.SHAHAR YAR <124666378+shaharyar522@users.noreply.github.com>
2025-02-14 14:45:06 +0500

    title changed

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Interactive CMS Dashboard</title>
+    <title>Interactive </title>
     <link rel="stylesheet" href="css/dashbord.css">
 </head>
 
