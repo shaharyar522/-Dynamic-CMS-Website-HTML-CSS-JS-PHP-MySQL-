9af6037bb04eb099ce98ddd51fdc940ebff074a1
shaharyar522 <shaharyar.khan4511@gmail.com>
2025-02-16 14:51:01 +0500

    commit

--- user_interface/index.php
+++ user_interface/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Website change the </title>
+    <title>My Website sddddddfffffffdsdfd</title>
     <link rel="stylesheet" href="css/main.css">
 </head>
 
